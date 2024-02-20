3443829100f462d24fe37759cb6667b146a79970
enewlin <elinewlin@gmail.com>
2024-02-19 23:51:24 -0600

    finally on track

--- assignment1_03/poke327/dj/dj.h
+++ assignment1_03/poke327/dj/dj.h
@@ -3,31 +3,23 @@
 
 
 # include <stdint.h>
+struct Heap {
+    int* arr;
+    int size;
+    int capacity;
+};
+ 
+// define the struct Heap name
+typedef struct Heap heap;
+ 
+// forward declarations
+heap* createHeap(int capacity, int* nums);
+void insertHelper(heap* h, int index);
+void heapify(heap* h, int index);
+int extractMin(heap* h);
+void insert(heap* h, int data);
 
-struct heap_node;
-typedef struct heap_node heap_node_t;
-
-typedef struct heap {
-  heap_node_t *min;
-  uint32_t size;
-  int32_t (*compare)(const void *key, const void *with);
-  void (*datum_delete)(void *);
-} heap_t;
-
-void heap_init(heap_t *h,
-               int32_t (*compare)(const void *key, const void *with),
-               void (*datum_delete)(void *));
-void heap_delete(heap_t *h);
-heap_node_t *heap_insert(heap_t *h, void *v);
-void *heap_peek_min(heap_t *h);
-void *heap_remove_min(heap_t *h);
-int heap_combine(heap_t *h, heap_t *h1, heap_t *h2);
-int heap_decrease_key(heap_t *h, heap_node_t *n, void *v);
-int heap_decrease_key_no_replace(heap_t *h, heap_node_t *n);
-    
-#define V 5
-
-
+void djkstra(char* board);
 
 
 
